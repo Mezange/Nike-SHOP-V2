ce1525744534fc12b94cc50a4eba3966573dc19d
Mezange <102484922+Mezange@users.noreply.github.com>
2022-07-10 00:58:01 +0200

    DARKMOD

--- js/script.js
+++ js/script.js
@@ -47,4 +47,20 @@
 /*=================== DARK / LIGHT THEME ===================*/
 /*==========================================================*/
 
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" :  "light");
 
+if (storedTheme)
+document.documentElement.setAttribute('data-theme', storedTheme)
+
+let toggle = document.getElementById('checkbox');
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute('data-theme');
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
